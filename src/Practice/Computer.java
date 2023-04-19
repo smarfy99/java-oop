c58164c91db72084c798a56727110f907bc6a082
Jiwon-Jeong99 <jjwzzy4643@gmail.com>
2023-04-19 23:26:22 +0900

    parameter

---
+++ src/Practice/Computer.java
@@ -0,0 +1,13 @@
+package Practice;
+
+public class Computer {
+    int sum(int ... values){
+        int sum = 0;
+
+        for(int i=0; i<values.length; i++){
+            sum += values[i];
+        }
+
+        return sum;
+    }
+}
---
+++ src/Practice/ComputerExample.java
@@ -0,0 +1,7 @@
+package Practice;
+
+public class ComputerExample {
+    public static void main(String[] args) {
+        Computer myCom = new Computer();
+    }
+}
