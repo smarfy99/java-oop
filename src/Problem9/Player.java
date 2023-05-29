fb7eb3654f3f99d05d5a11e0b53366e562feb246
Jiwon-Jeong99 <jjwzzy4643@gmail.com>
2023-05-29 17:13:55 +0900

    Problem9 startg

---
+++ src/Problem9/Player.java
@@ -0,0 +1,11 @@
+package Problem9;
+
+public class Player {
+    private String name;
+    private double speed;
+    private String[] tools;
+
+    String getName() {
+        return name;
+    }
+}
